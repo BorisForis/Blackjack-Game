237dbbbfc537ac21a81453dc9ea5eb7fa208b01f
Mircea-Dumitru Duca <mircealike2013@gmail.com>
2024-06-04 20:36:45 +0300

    Please go to main

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            boxShadow: {
+                button1: '7px 5px 56px -10px #f44336',
+                button2: '7px 5px 56px -14px #f44336',
+            },
+        },
+    },
+    plugins: [],
+};
+export default config;
